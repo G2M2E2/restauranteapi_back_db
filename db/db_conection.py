1525a9c246f6d1aff3a2d4f09287d4f606b0c6dc
Juan Navarro <jupanze@gmail.com>
2020-12-18 20:40:00 -0500

    Conexion BD Heroku

--- db/db_conection.py
+++ db/db_conection.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 #Creando Motor y Conexion con la Base de Datos
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:juan1234@localhost:5432/Restaurante"
+SQLALCHEMY_DATABASE_URL = "postgres://fuloieuiozijtk:085e261dba0cb1b01554c7c6cdcc61ead243600e24e8f473f1711a43c4cf8ee3@ec2-34-194-198-238.compute-1.amazonaws.com:5432/dik18adq13nsl"
 engine                  = create_engine(SQLALCHEMY_DATABASE_URL)
 
 #Creacion de la Sesion 
